bedac27261754ef6af1eba1d55f24ebf5b147471
Иванов Михаил <MEIvanov@alfabank.ru>
2024-12-18 11:18:05 +0300

    Initial Commit

---
+++ Financial Circle/Financial_CircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Financial_CircleApp.swift
+//  Financial Circle
+//
+//  Created by Mikhail Ivanov on 18.12.24.
+//
+
+import SwiftUI
+
+@main
+struct Financial_CircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
